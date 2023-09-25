518de0ca6bcc8caff85e930b0fda2fcb0992f042
Phan Van Tuan <ptuan123987@gmail.com>
2023-09-26 00:43:05 +0700

    First commit

---
+++ src/main/java/com/example/clientServer/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.clientServer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE"));
+//        config.setAllowedOrigins(Collections.singletonList("https://library-management.up.railway.app"));
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
